d77588e835818a50b7bacb8d4c627dc4aa5c8ee1
Ronny Eichler <ronny.eichler@gmail.com>
2016-03-08 23:59:55 +0100

    Cleanup of local plotting file

--- mk_plot.py
+++ mk_plot.py
@@ -3,134 +3,117 @@
 from Stopwatch import Stopwatch
 startup = Stopwatch('Imports')
 startup.start()
-import time
-startup.event('time')
 import pandas as pd
 startup.event('pandas')
 import matplotlib.pyplot as plt
 startup.event('matplotlib')
 import sqlite3
 startup.event('sqlite3')
-import numpy as np
-startup.event('numpy')
 import subprocess
 startup.event('subprocess')
-import logging
-startup.event('logging')
+# import time
+# startup.event('time')
+# import numpy as np
+# startup.event('numpy')
+# import logging
+# startup.event('logging')
 startup.report()
 
-LOOP = 0
 UPLOAD = True
-HOST = 'chuck'
-DB_PATH = '/home/reichler/telemetry.db'
-IMG_OUT = 'plot.png'
+SENSOR_HOST = 'chuck'
+LOCAL_DB_PATH = 'db/telemetry.db'
+LOCAL_IMG_PATH = 'plot.png'
+REMOTE_IMG_PATH = '~/projects/static/'
+REMOTE_HOST = 'lychnobite.me'
 
-def read_df(db_path, host, resample=False, from_id=0, sw=None):
-    # TODO: Select only current host
-    if sw is None:
-        sw = Stopwatch('Dataframe loading and processing')
 
-    with sqlite3.connect(db_path) as con:
-        cur = con.cursor()
-        cur.execute('SELECT MAX(id) FROM telemetry;')
-        last_id = cur.fetchone()[0]
-        
-        # if we didn't get anything from that query, return current id
-        # may have new data later...
-        if from_id > last_id:
-            return None, from_id, sw
-        
-        df = pd.read_sql_query('SELECT * FROM telemetry where id>={} AND id<={};'.format(from_id, last_id), con)
+def read_df(from_id=0, **kwargs):
+    sw = kwargs['sw'] if 'sw' in kwargs else Stopwatch('Data frame loading')
+
+    with sqlite3.connect(LOCAL_DB_PATH) as con:
+        # TODO: Select only current host
+        df = pd.read_sql_query('SELECT * FROM telemetry WHERE id>={id};'.format(id=from_id), con)
     sw.event('SQL raw data into df')
 
+    return df, sw
+
+
+def prepare_df(df, resample='5min', **kwargs):
+    sw = kwargs['sw'] if 'sw' in kwargs else Stopwatch('Data frame preparation')
+
     # timestamps as datetime array
-    #df.set_index('timestamp')
     df['timestamp'] = pd.to_datetime(df['timestamp'], unit='s')
-    sw.event('convert timestamps to datetime')
-    
+    sw.event('Timestamps to datetime')
+
     # type as category (sensor type)
     df['type'] = df['type'].astype('category')
-    newcats = [sensors_rev[c] for c in df['type'].cat.categories]
-    df['type'].cat.categories = newcats
-    sw.event('type as category')
-  
-    pivoted = df.pivot(index = 'timestamp', columns='type', values='value')
-    sw.event('pivot table')
-    
-    if resample:
-        pivoted = pivoted.resample(resample, how='mean')
-        sw.event('resampling')
+    df['type'].cat.categories = [sensors_rev[c] for c in df['type'].cat.categories]
+    sw.event('Type as category')
 
-    # make temperature more visible # hack, hack
-    pivoted['temp'] *=10
-    sw.event('scale temperature')
+    # Pivot the table for fun and profit
+    # NOTE: Will fail when duplicate entries (i.e. types must be unique per timestamp)
+    df = df.pivot(index='timestamp', columns='type', values='value')
+    sw.event('Pivot table')
+
+    # Resample to given time interval
+    if resample:
+        df = df.resample(resample, how='mean')
+        sw.event('Resampling to {}'.format(resample))
 
     # adjust time zone
-    pivoted = pivoted.tz_localize('UTC').tz_convert('Europe/Amsterdam')
-    sw.event('adjust timezone')
-    
-    return pivoted, last_id, sw
+    df = df.tz_localize('UTC').tz_convert('Europe/Amsterdam')
+    sw.event('Timezone adjustment')
+
+    return df, sw
 
 
-def make_plot(df, plot_path, sw=None):
-    if sw is None:
-        sw = Stopwatch('Plotting')
+def make_plot(df, plot_path=LOCAL_IMG_PATH, **kwargs):
+    sw = kwargs['sw'] if 'sw' in kwargs else Stopwatch('Plotting')
     
     p = df.plot()
-    sw.event('plot')
+    sw.event('Plotting of the df')
 
     fig = plt.gcf()
     fig.set_size_inches(20, 5)
 
     elapsed = sw.elapsed()
-    p.annotate('{:.0f} s'.format(elapsed), xy=(1, 0), xycoords='axes fraction', fontsize=10, xytext=(0, -15),
-                              textcoords='offset points', ha='right', va='top')
+    p.annotate('{:.1f} s'.format(elapsed), xy=(1, 0), xycoords='axes fraction', fontsize=10, xytext=(0, -15),
+               textcoords='offset points', ha='right', va='top')
     fig.tight_layout()
     fig.savefig(plot_path, dpi=150)
-    sw.event('savefig')
+    sw.event('Saving figure')
     plt.close()
     plt.clf()
-    sw.event('close plt')
-    
+
     return sw
+
 
 def get_sensors(db_path, host):
     with sqlite3.connect(db_path) as con:
         cur = con.cursor()
         cur.execute("SELECT * FROM sensors WHERE host='{}';".format(host))
-        sensors = {s[2]: s[0] for s in cur.fetchall()}
-    return sensors
+        return {s[2]: s[0] for s in cur.fetchall()}
+
+
+def upload(img_path=LOCAL_IMG_PATH, dst=REMOTE_HOST):
+    if UPLOAD:
+        try:
+            rc = subprocess.check_output(['scp', img_path, dst])
+            print rc
+        except subprocess.CalledProcessError, e:
+            print "Failed to upload: {}".format(e)
 
 
 if __name__ == "__main__":
-    sensors = get_sensors(DB_PATH, HOST)
-    sensors_rev = {v:k for k, v in sensors.iteritems()}
-    
-    last_id = 0
-    telemetry = None
-    
-    while True:
-        print "read from ", last_id
-        fragment, ret_id, sw = read_df(DB_PATH, HOST, resample='5min', from_id=last_id)
-        print 'read until', ret_id
-        if fragment is not None:
-            last_id = ret_id + 1
-            if telemetry is None:
-                telemetry = fragment
-            else:
-                print "Adding {} lines.".format(len(fragment))
-                telemetry.append(fragment)
-            make_plot(telemetry, IMG_OUT, sw)
-            if UPLOAD:
-                try:
-                    rc = subprocess.check_output(['scp', 'plot.png', 'lychnobite.me:~/projects/static/'])
-                    print rc
-                except subprocess.Error, e:
-                    print "Failed to upload: {}".format(e)
+    stopwatch = Stopwatch('Loading and plotting ALL THE DATA!')
 
-        sw.report()
-        time.sleep(LOOP)
+    sensors = get_sensors(LOCAL_DB_PATH, SENSOR_HOST)
+    sensors_rev = {v: k for k, v in sensors.iteritems()}
 
-        sw = None
-        if not LOOP:
-            break
+    data, stopwatch = read_df(resample='5min', sw=stopwatch)
+    data, stopwatch = prepare_df(data, sw=stopwatch)
+
+    make_plot(data, sw=stopwatch)
+    upload(LOCAL_IMG_PATH, ":".join([REMOTE_HOST, REMOTE_IMG_PATH]))
+
+    stopwatch.report()

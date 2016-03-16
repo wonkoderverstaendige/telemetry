a03320ceee6993b9d1945853fb019c58faeb98c1
Ronny Eichler <ronny.eichler@gmail.com>
2016-03-16 23:21:07 +0100

    Broke bed/record

--- bed/record.py
+++ bed/record.py
@@ -14,7 +14,7 @@ def timestamp():
 bufsize = 1 if DEBUG else -1
 with open('db/rec.csv', 'a', bufsize) as f:
     writer = csv.writer(f)
-    with serial.Serial('/dev/ttyACM0', 57600, timeout=1.0) as ser:
+    with serial.Serial('/dev/ttyUSB0', 57600, timeout=1.0) as ser:
         # Empty whatever is in the buffer
         ser.flushInput()
         while ser.inWaiting():
@@ -22,11 +22,9 @@ with open('db/rec.csv', 'a', bufsize) as f:
         
         while True:
             values = []
-            for _ in range(50):
-                line = ser.readline().strip()
-                try:
-                    values.append(int(line))
-                except ValueError:
-                    print str(dt.now()), 'NaN:', line
-            print timestamp(), min(values), max(values)
-            writer.writerow([timestamp(), min(values), max(values)])
+	    ser.write('a')
+		while ser.inWaiting():
+		line = ser.readline().strip()
+		print line
+		time.sleep(100)
+            writer.writerow([timestamp(), line])

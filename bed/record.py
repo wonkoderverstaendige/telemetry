ba6b1d129e60247f501b06072448a1478ed99137
Ronny Eichler <ronny.eichler@gmail.com>
2016-03-17 02:40:33 +0100

    Include temperature in bed telemetry

--- bed/record.py
+++ bed/record.py
@@ -11,8 +11,8 @@ def timestamp():
     ts = dt.now()
     return time.mktime(ts.timetuple())+(ts.microsecond/1e6)
 
-bufsize = 1 if DEBUG else -1
-with open('db/rec.csv', 'a', bufsize) as f:
+buffer_size = 1 if DEBUG else 4*2**10
+with open('db/rec.csv', 'a', buffer_size) as f:
     writer = csv.writer(f)
     with serial.Serial('/dev/ttyUSB0', 57600, timeout=1.0) as ser:
         # Empty whatever is in the buffer
@@ -21,10 +21,27 @@ with open('db/rec.csv', 'a', bufsize) as f:
             ser.read()
         
         while True:
-            values = []
-	    ser.write('a')
-		while ser.inWaiting():
-		line = ser.readline().strip()
-		print line
-		time.sleep(100)
-            writer.writerow([timestamp(), line])
+            data = {'temp': [],
+                    'strain': []}
+            for n in range(300):
+                ser.write('a')
+                time.sleep(.100)
+                while ser.inWaiting():
+                    try:
+                        sensor, value = ser.readline().rstrip().split(':')
+                        value = float(value)
+                        data[sensor].append(value)
+                    except (KeyError, ValueError),  error:
+                        print error
+            for key, values in data.items():
+                ts = timestamp()
+                if key == 'strain':
+                    hi = [ts, 'strain_hi', max(values)]
+                    lo = [ts, 'strain_lo', min(values)]
+                    print hi, '\n', lo
+                    writer.writerows([hi, lo])
+                else:
+                    row = [ts, key, sum(values)*1./len(values)]
+                    print row
+                    writer.writerow(row)
+

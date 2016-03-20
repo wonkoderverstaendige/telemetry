2ca271841d6e6442d61518f80ee2135045a37612
Ronny Eichler <ronny.eichler@gmail.com>
2016-03-20 08:34:30 +0100

    Various IO and utility functions packed away

--- util/db_io.py
+++ util/db_io.py
@@ -2,74 +2,64 @@
 
 import argparse
 import sqlite3
-import subprocess
 
-import numpy
 import pandas
 
-import Stopwatch
 from misc import get_local_config
 
 
-def sql_table2df(db, table, from_id=0, **kwargs):
+def csv_table2df(csv_file):
+    df = pandas.read_csv(csv_file, names=['timestamp', 'type', 'value'])
+    df.type.loc[df.type == 'strain_lo'] = 6
+    df.type.loc[df.type == 'strain_hi'] = 7
+    df.type.loc[df.type == 'temp'] = 8
+    df.type.astype('uint8')
+    return df
+    # df.index = pd.to_datetime(df.index, unit='s').tz_localize('UTC').tz_convert('Europe/Amsterdam')
+
+
+def sql_table2df(sql_con, table, start=0, end=None, **kwargs):
+    """
+    Read table from connection into a pandas data frame.
+    :param sql_con: sqlite3 connection object. NOTE: Not cursor!
+    :param table: table to read from
+    :param start: Start, in unix epoch time, default = 0
+    :param end: End, in unix epoch time, default = None
+    :param kwargs: **kwargs
+    :return:
+    """
     # TODO: Select only current host
-    sw = kwargs['sw'] if 'sw' in kwargs else Stopwatch.Stopwatch('Data frame loading')
+    query = 'SELECT timestamp, type, value FROM {table}'.format(table=table)
+    if end is not None:
+        assert start < end, "End ({}) must be larger then start ({})".format(end, start)
+    if start or end:
+        condition = 'rowid>{}'.format(start) if start else ''
+        condition += ' AND ' if start and end else ''
+        condition += 'rowid<{}'.format(end) if end else ''
+        query += ' WHERE({:s})'.format(condition)
+    return pandas.read_sql_query(query, sql_con)
 
-    with sqlite3.connect(db) as con:
-        df = pandas.read_sql_query('SELECT * FROM {t} WHERE id>={id}'.format(t=table, id=from_id), con)
-    sw.event('SQL raw data into df')
 
-    return df, sw
-
-
-def prepare_df(df, sensors, resample='5min', **kwargs):
-    sensors = sql_sensor_description()
-    sw = kwargs['sw'] if 'sw' in kwargs else Stopwatch.Stopwatch('Data frame preparation')
-
-    # timestamps as datetime array
-    df['timestamp'] = pandas.to_datetime(df['timestamp'], unit='s')
-    sw.event('Timestamps to datetime')
+def prepare_df(df):
+    df.timestamp = df.timestamp.astype("datetime64[s]")
+    df.set_index('timestamp', inplace=True)
+    df.tz_localize('UTC', copy=False) \
+      .tz_convert('Europe/Amsterdam', copy=False)
 
     # TODO: Translate category elsewhere?
     # type as category (sensor type)
     # df['type'] = df['type'].astype('category')
     # df['type'].cat.categories = [sensors_rev[c] for c in df['type'].cat.categories]
-    # sw.event('Type as category')
 
     # Pivot the table for fun and profit
     # NOTE: Will fail when duplicate entries (i.e. types must be unique per timestamp)
-    df = df.pivot(index='timestamp', columns='type', values='value')
-    sw.event('Pivot table')
+    # df = df.pivot(index='timestamp', columns='type', values='value')
 
     # Resample to given time interval
-    if resample:
-        df = df.resample(resample, how='mean')
-        sw.event('Resampling to {}'.format(resample))
-
-    df.temp[(df['light'] > 250) & (df.index < '2016-03-10 07:03:57.603722+01:00')] = numpy.NaN
-    sw.event("Remove false temp values.")
-
-    # adjust time zone
-    df = df.tz_localize('UTC').tz_convert('Europe/Amsterdam')
-    sw.event('Timezone adjustment')
-
-    return df, sw
-
-
-# def sql_sensor_ids(cursor, host=None, table='sensors'):
-#     """
-#     Get the name and ids of sensors in the db of the cursor.
-#     :param cursor: sqlite3 db cursor.
-#     :param host: Restrict selection to sensors belonging to host/node.
-#     :param table: Name of table containing sensor descriptions. (default='sensors')
-#     :return: Dictionary of {sensorID: sensorName}
-#     """
-#     try:
-#         condition = "WHERE host='{}'".format(host) if host else ''
-#         cursor.execute("SELECT * FROM {}".format(table) + condition)
-#         return {s[0]: s[2] for s in cur.fetchall()}
-#     except sqlite3.Error, e:
-#         print "Error: {}".format(e)
+    # if resample:
+    #     df = df.resample(resample, how='mean')
+    # df.temp[(df['light'] > 250) & (df.index < '2016-03-10 07:03:57.603722+01:00')] = numpy.NaN
+    return df
 
 
 def sql_sensor_description(cursor, host=None, table='sensors'):
@@ -81,7 +71,7 @@ def sql_sensor_description(cursor, host=None, table='sensors'):
     :return: Dictionary of {sensorID: (host, factor, unit, description)}
     """
     try:
-        query = "SELECT * FROM {} ".format(table) + 'WHERE host="{}"'.format(host) if host else ''
+        query = "SELECT * FROM {} ".format(table) + ('WHERE host="{}"'.format(host) if host else '')
         cursor.execute(query)
         return {s[0]: (s[1], s[2], s[3], s[4], s[5]) for s in cursor.fetchall()}
     except sqlite3.Error, e:
@@ -135,9 +125,9 @@ def sql_check_db(*args, **kwargs):
     :param kwargs: Name of database table.
     :return: Number of duplicate rows found, (0 if none) and None if assertion fails.
     """
-    consistency = sql_check_consistency(*args)
+    consistency = sql_check_consistency(*args, **kwargs)
     assert consistency == u'ok', "DB consistency check failed: {}".format(consistency)
-    num_duplicates = sql_duplicates_count(*args)
+    num_duplicates = sql_duplicates_count(*args, **kwargs)
     assert num_duplicates == 0, "{} duplicate entries detected!".format(num_duplicates)
     return num_duplicates
 
@@ -151,7 +141,7 @@ if __name__ == "__main__":
     db = cli_args.db if cli_args.db else config['paths']['db']
     node = cli_args.node if cli_args.node else config['host']['node']
 
-    with sqlite3.connect(db, detect_types=sqlite3.PARSE_DECLTYPES) as con:
+    with sqlite3.connect(db) as con:
         cur = con.cursor()
         assert sql_check_db(cur) is 0
         print "Id\t host\t type\t f*unit\t description"
--- util/misc.py
+++ util/misc.py
@@ -12,22 +12,31 @@ def rev_dict(d):
         print error
 
 
-def upload(src, dst):
+def upload_scp(src, dst):
+    """
+    Upload src file to dst via scp
+    :param src:
+    :param dst:
+    :return: None if failed, else return code
+    """
     try:
         return subprocess.check_output(['scp', src, dst])
-    except subprocess.CalledProcessError, e:
-        print "Failed to upload: {}".format(e)
-        return {}
+    except subprocess.CalledProcessError, error:
+        print "Failed to upload: {}".format(error)
+        return error
 
 
 def get_local_config():
-    """Return localhost.ini configuration as dictionary"""
+    """
+    Grab config as dictionary.
+    :return: dict() of local configuration
+    """
+    this_file = os.path.dirname(__file__)
     config = ConfigParser.SafeConfigParser()
-    config.read(os.path.join(os.path.dirname(__file__), '../config', 'localhost.ini'))
+    config.read(os.path.join(this_file, '../config', 'localhost.ini'))
     cfg_dict = {s: dict(config.items(s)) for s in config.sections()}
 
     # make the relative paths into a universally useful absolute paths
     for name, path in cfg_dict['paths'].items():
-        cfg_dict['paths'][name] = os.path.abspath(path)
-
+        cfg_dict['paths'][name] = os.path.abspath(os.path.join(this_file, path))
     return cfg_dict

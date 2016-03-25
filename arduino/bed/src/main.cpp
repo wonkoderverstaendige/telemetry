23b1d7951759908464dd2d643a48d4caa603072e
Ronny Eichler <ronny.eichler@gmail.com>
2016-03-25 06:34:55 +0100

    Update chuck and bed to array of sensors

--- arduino/bed/src/main.cpp
+++ arduino/bed/src/main.cpp
@@ -2,27 +2,40 @@
 #include <OneWire.h>
 #include "OneWireSensor.h"
 #include "AnalogSensor.h"
+#include "DigitalSensor.h"
 
-#define NO_DIGITAL 0
+#define NUM_SENSORS 3
 #define ONE_WIRE_BUS 8
 #define STRAIN_ADC A5
+#define MOTION_DIG 2
 
 #define SECOND 1000
 #define MINUTE 60*1000l
 
 OneWire ow_bus(ONE_WIRE_BUS);
-OneWireSensor DS_temp("temp", &ow_bus, 10, 6*SECOND, 1*MINUTE);  // DS18b20
+// 0: 2 strain gauges in wheatstone bridge
+AnalogSensor AS_strain("strain", 20, 500, 10*SECOND, STRAIN_ADC);
+// 1: DS18b20
+OneWireSensor OW_temp("temp", &ow_bus, 10, 6*SECOND, 1*MINUTE);
+// 2: PIR with digital output
+DigitalSensor DS_motion("motion", 1, 0, 0, MOTION_DIG);
 
-AnalogSensor AS_strain("strain", 20, 500, 10*SECOND, STRAIN_ADC, NO_DIGITAL);
+GenericSensor* sensors[NUM_SENSORS] = {&OW_temp, &AS_strain, &DS_motion};
 
-unsigned long currentMillis;
+void motion() {
+  Serial.println("motion!");
+  sensors[2]->tick(millis());
+}
+
 void setup() {
-  DS_temp.setScaleFactor(1.0f/16);
   Serial.begin(57600);
+
+  sensors[2]->setScaleFactor(1.0f/16);
+  attachInterrupt(digitalPinToInterrupt(MOTION_DIG), motion, CHANGE);
 }
 
 void loop() {
-  currentMillis = millis();
-  AS_strain.tick(currentMillis);
-  DS_temp.tick(currentMillis);
+  // ping ALL the sensors to have 'em do their thing
+  // NOTE: Skipping last sensor, we call that one via interrupt
+  for (byte i=0; i<NUM_SENSORS-1; i++) sensors[i]->tick(millis());
 }
--- arduino/chuck/src/main.cpp
+++ arduino/chuck/src/main.cpp
@@ -1,43 +1,40 @@
 #include <Arduino.h>
+#include <OneWire.h>
+#include "GenericSensor.h"
 #include "AnalogSensor.h"
-#include "OneWire.h"
 #include "OneWireSensor.h"
 
+#define NUM_SENSORS 4
 #define SOIL_PIN 7
 #define SOIL_ADC A0
 #define TEMP_ADC A1
 #define LIGHT_ADC A2
-#define NO_DIGITAL 0
 #define ONE_WIRE_BUS 8
 
 #define SECOND 1000
 #define MINUTE 60*1000l
 
-// two sticks in the ground
-AnalogSensor AS_soil("soil", 3, 10*MINUTE, 30*MINUTE, SOIL_ADC, SOIL_PIN);
-// LM35 analog temperature sensor
-AnalogSensor AS_temp("temp", 10, 6*SECOND, 1*MINUTE, TEMP_ADC, NO_DIGITAL);
-// LDR
-AnalogSensor AS_light("light", 10, 6*SECOND, 1*MINUTE, LIGHT_ADC, NO_DIGITAL);
-
-// DS18b20 digital temperature sensor
 OneWire ow_bus(ONE_WIRE_BUS);
-OneWireSensor DS_temp("temp", &ow_bus, 10, 6*SECOND, 1*MINUTE);
+// 0, two sticks in the ground
+AnalogSensor AS_soil("soil", 3, 10*MINUTE, 30*MINUTE, SOIL_ADC);
+// 1, LM35 analog temperature sensor
+AnalogSensor AS_temp("temp", 10, 6*SECOND, 1*MINUTE, TEMP_ADC);
+// 2, LDR
+AnalogSensor AS_light("light", 10, 6*SECOND, 1*MINUTE, LIGHT_ADC);
+// 3, DS18b20 digital temperature sensor
+OneWireSensor OW_temp("ds_temp", &ow_bus, 10, 6*SECOND, 1*MINUTE);
 
-unsigned long currentMillis;
+GenericSensor* sensors[NUM_SENSORS] = {&AS_soil, &AS_temp, &AS_light, &OW_temp};
 
 void setup() {
-  AS_temp.setScaleFactor(0.488f);
-  DS_temp.setScaleFactor(1.0f/16);
   Serial.begin(57600);
+
+  sensors[0]->auxPin(SOIL_PIN);
+  sensors[1]->setScaleFactor(0.488f);
+  sensors[3]->setScaleFactor(1.0f/16);
 }
 
 void loop() {
-    currentMillis = millis();
-
-    // ping ALL the sensors to have 'em do their thing
-    AS_soil.tick(currentMillis);
-    AS_temp.tick(currentMillis);
-    AS_light.tick(currentMillis);
-    DS_temp.tick(currentMillis);
+  // ping ALL the sensors to have 'em do their thing
+  for (byte i=0; i<NUM_SENSORS; i++) sensors[i]->tick(millis());
 }

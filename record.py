a240d8b722c2d303d938743e0a46f80d61fb68d3
Ronny Eichler <ronny.eichler@gmail.com>
2016-03-26 04:28:08 +0100

    Generalized record.py

--- record.py
+++ record.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 from __future__ import division
 import time
 import os
@@ -22,23 +24,33 @@ def clear_serial(ser):
     # Empty whatever is still stuck in the buffer
     ser.flushInput()
     while ser.inWaiting():
-        ser.read()
+        # wait for the end of the first line
+        ser.readline()
 
 
 def loop(writer, ser):
     while True:
         try:
-            sensor, value = ser.readline().rstrip().split(':')
+            line = ser.readline()
+            sensor, value = line.rstrip().split(':')
             value = float(value)
         except (KeyError, ValueError), error:
-            print error
+            pass
+            # print error
         else:
-            row = [timestamp(), sensor, value]
+            row = [timestamp(), sensor, '{:.2f}'.format(value)]
             print row
             writer.writerow(row)
 
 
 if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser(description="Save serial data to csv file.")
+    parser.add_argument('-p', '--port', help='Serial port path', default=SERIAL_PATH)
+    parser.add_argument('-b', '--baud', help='Serial baud rate', default=57600)
+    parser.add_argument('-r', '--timeout', help='Serial baud rate', default=10.0)
+    cli_args = parser.parse_args()
+
     assert os.path.exists(SERIAL_PATH)
     assert os.path.exists(LOCAL_DB_PATH)
 
@@ -47,6 +59,6 @@ if __name__ == "__main__":
     print os.path.join(LOCAL_DB_PATH, filename)
     with open(os.path.join(LOCAL_DB_PATH, filename), 'a', buffer_size) as f:
         csv_writer = csv.writer(f)
-        with serial.Serial(SERIAL_PATH, 57600, timeout=1.0) as serial_port:
+        with serial.Serial(cli_args.port, cli_args.baud, timeout=cli_args.timeout) as serial_port:
             clear_serial(serial_port)
             loop(csv_writer, serial_port)
